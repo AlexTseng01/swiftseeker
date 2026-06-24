de48e43a4513bc8a7f26ea692384d7e879d37c68
AlexTseng01 <atseng1@umbc.edu>
2026-06-24 02:48:54 -0400

    renamed main driver app

--- FileTracerApp.java
+++ FileTracerApp.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.nio.file.*;
 
-public class HomingApp {
+public class FileTracerApp {
     public static void main(String[] args) {
         System.out.println("Hello World...");
     }
