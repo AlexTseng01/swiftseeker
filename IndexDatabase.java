ae2dc6b1d020d23158ce0054bf4f57e8e264d4dc
AlexTseng01 <atseng1@umbc.edu>
2026-06-25 17:00:03 -0400

    implemented IndexDatabase

--- IndexDatabase.java
+++ IndexDatabase.java
@@ -18,7 +18,7 @@ public class IndexDatabase {
     public IndexDatabase() {
         try (Connection connection = DriverManager.getConnection(DB_URL); Statement stmt = connection.createStatement()) {
             String createTableSQL = "CREATE TABLE IF NOT EXISTS files (filename TEXT, filepath TEXT UNIQUE);";
-        stmt.execute(createTableSQL);
+            stmt.execute(createTableSQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -26,11 +26,35 @@ public class IndexDatabase {
 
     // Literally just an add method for the SQLite db
     public void insert(Path file) {
-
+        String sql = "INSERT OR IGNORE INTO files(filename, filepath) VALUES(?, ?)";
+        
+        try (Connection connection = DriverManager.getConnection(DB_URL)) {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, file.getFileName().toString());
+            pstmt.setString(2, file.toString());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     // Also literally just a get method for the SQLite db
     public List<String> search(String query) {
-        return null;
+        List<String> result = new ArrayList<>();
+        String sql = "SELECT filepath FROM files WHERE filename LIKE ?";
+        
+        try (Connection connection = DriverManager.getConnection(DB_URL)) {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, "%" + query + "%");
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                result.add(rs.getString("filepath"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
